1a7f772a647d843261df728477e65439c945caa9
Rahmat_Alrafi_Trikada <63140592+RahmatAT11@users.noreply.github.com>
2021-05-02 11:04:20 +0700

    Menambah mekanik burung meledak ketika bersentuhan dengan obyek

---
+++ Assets/Scripts/Owl.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+
+public class Owl : Bird
+{
+    [SerializeField]
+    private float _explosionRadius;
+    [SerializeField]
+    private float _explosionPower;
+
+    // Burung akan meledak ketika bersentuhan dengan objek lain
+    public void Explode()
+    {
+        Vector2 explosionPos = transform.position;
+        Collider2D[] colliders = Physics2D.OverlapCircleAll(explosionPos, _explosionRadius);
+
+        foreach (Collider2D hit in colliders)
+        {
+            Rigidbody2D rigidbody2D = hit.GetComponent<Rigidbody2D>();
+            Vector2 dir = hit.gameObject.transform.position - transform.position;
+
+            if (rigidbody2D != null)
+            {
+                rigidbody2D.AddForce(dir * _explosionPower, ForceMode2D.Impulse);
+            }
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D col)
+    {
+        Explode();
+    }
+}
